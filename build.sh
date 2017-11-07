b4a7a9a240e5005d7a655c919e5eb182139c8ff9
Joao Aparicio <jpmn.aparicio@gmail.com>
2017-11-07 18:13:19 +0000

    First

---
+++ build.sh
@@ -0,0 +1,3 @@
+rm readft.xpi
+cd readft
+zip -r ../readft.xpi *
---
+++ readft/background.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var ref = 'https://www.facebook.com/';
+
+browser.webRequest.onBeforeSendHeaders.addListener(function (details)
+{
+  var requestHeaders = details.requestHeaders;
+
+  var ref_is_set = false;
+
+  for (var i = 0; i < requestHeaders.length; i++) {
+    if (requestHeaders[i].name === 'Referer') {
+      requestHeaders[i].value = ref;
+      ref_is_set = true;
+    }
+  }
+
+  if (!ref_is_set) {
+    requestHeaders.push({name: 'Referer', value: ref});
+  }
+
+  return {requestHeaders: requestHeaders};
+},
+{
+  urls: ['<all_urls>']
+}, ['blocking', 'requestHeaders']);
